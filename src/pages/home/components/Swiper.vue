3a4e8fa0b93d4cd7b3ee604165f325447f30f595
JettWoo <jett.woo@foxmail.com>
2020-03-02 19:40:32 +0800

    首页轮播图完成

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,54 @@
+<template>
+<div class="wrapper">
+  <swiper :options="swiperOption" ref="mySwiper">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key='item.id'>
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'https://imgs.qunarzz.com/vc/44/e9/86/95bc36c9e1c06ebd68bdfe222e.jpg_92.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://imgs.qunarzz.com/vc/ad/7c/fe/037d1a400372ff57b0030d1d1c.jpg_92.jpg'
+        },
+        {
+          id: '0003',
+          imgUrl: 'https://imgs.qunarzz.com/vc/24/36/10/3fc94d042ae62de380581d4c98.jpg_92.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background: #fff !important
+.wrapper
+  overflow: hidden
+  width: 100%
+  height: 0
+  padding-bottom: 30.47%
+  .swiper-img
+    width: 100%
+</style>
