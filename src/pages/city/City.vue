2cef156745e95ac507888d8f715ec3a903dc227d
JettWoo <jett.woo@foxmail.com>
2020-03-03 14:22:37 +0800

    add city-search layout

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <city-header></city-header>
+    <city-search></city-search>
     这里是城市选择页面
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
