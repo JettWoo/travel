9d88ebbec562168230b60a31f68c9424f0a898b5
JettWoo <jett.woo@foxmail.com>
2020-03-04 09:01:09 +0800

    finish city css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul class="list">
+    <li class="item">
+      A
+    </li>
+    <li class="item">
+      A
+    </li>
+    <li class="item">
+      A
+    </li>
+    <li class="item">
+      A
+    </li>
+    <li class="item">
+      A
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+.list
+  position: fixed;
+  right: 0;
+  top: 1.68rem;
+  width: .4rem;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  align-item: center;
+  justify-content: center;
+  .item
+    text-align: center;
+    line-height: .4rem
+    color: $bgColor;
+</style>
