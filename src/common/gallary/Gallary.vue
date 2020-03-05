4299ca5147ce8ca4bdee4e451dad7d11198dcb68
JettWoo <jett.woo@foxmail.com>
2020-03-05 21:42:24 +0800

    add detail route banner

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="container" @click="handleGallaryClick">
+    <div class="wrapper">
+      <swiper ref="mySwiper" :options="swiperOption" v-if="imgs.length > 0"> <!--   -->
+        <!-- slides -->
+        <swiper-slide v-for="(item, index) of imgs" :key='index'> <!--   -->
+          <img
+            class="swiper-img"
+            :src="item" alt="">
+        </swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallary',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'fraction',
+          observeParents: true,
+          observe: true
+        }
+      }
+    }
+  },
+  methods: {
+    handleGallaryClick () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.container >>> .swiper-container
+  overflow: inherit
+.container
+  position: fixed
+  left: 0
+  right: 0
+  top: 0
+  bottom: 0
+  background: #000
+  z-index: 999
+  display: flex
+  flex-direction: column
+  justify-content: center
+  .wrapper
+    background: #fff
+    height: 0
+    width: 100%
+    padding-bottom: 100%
+    .swiper-img
+      width: 100%
+    .swiper-pagination
+      color: #fff
+      bottom: -1rem
+</style>
